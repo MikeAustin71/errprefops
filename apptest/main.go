cac6b0943a2d699b03fa7093cc6ef517bd70d7ea
Mike Rapp <jordy.rascal@protonmail.com>
2021-01-27 13:10:22 -0600

    Test is passing

--- apptest/main.go
+++ apptest/main.go
@@ -3,15 +3,40 @@ package main
 import (
 	"fmt"
 	"github.com/MikeAustin71/errprefops/errpref"
+	"strings"
 )
 
 func main() {
+	TestMain01()
+}
 
-	ePrefix := "Tx1.Something()\nTx2.SomethingElse()\nTx3.DoSomething()"
+func TestMain02() {
+	tStr := "Test01%Test02%Test03%Test04%Test05%Test06"
 
-	ePrefix = errpref.ErrPref{}.New(ePrefix, "Tx4.TrySomethingNew()")
+	fmt.Printf("Test String: %v\n", tStr)
+
+	tStrArray := strings.Split(tStr, "%")
+
+	for _, s := range tStrArray {
+		fmt.Printf("%v\n", s)
+	}
+
+}
+
+func TestMain01() {
+
+	ePrefix := "Tx1.Something()\nTx2.SomethingElse()\nTx3.DoSomething()\nTx4() - Tx5()\nTx6.DoSomethingElse()\n"
+
+	ePrefix = errpref.ErrPref{}.New(ePrefix, "Tx7.TrySomethingNew()")
 
 	ePrefix = errpref.ErrPref{}.AddContext(ePrefix, "something->newSomething")
+
+	ePrefix = errpref.ErrPref{}.New(ePrefix, "Tx8.TryCombination()")
+	ePrefix = errpref.ErrPref{}.New(ePrefix, "Tx9.TryHammer()")
+	ePrefix = errpref.ErrPref{}.AddContext(ePrefix, "x->y")
+	ePrefix = errpref.ErrPref{}.New(ePrefix, "Tx10.X()")
+	ePrefix = errpref.ErrPref{}.New(ePrefix, "Tx11.TryAnything()")
+	ePrefix = errpref.ErrPref{}.New(ePrefix, "Tx12.TryToGetAJob()")
 
 	ePrefix = errpref.ErrPref{}.FmtString(ePrefix)
 
--- errpref/errprefquark.go
+++ errpref/errprefquark.go
@@ -41,42 +41,160 @@ func (ePrefQuark *errPrefQuark) formatErrPrefix(
 
 	errPrefix = strings.ReplaceAll(errPrefix, " - ", "\n")
 
+	errPrefix += "\n"
+
 	errPrefixContextCollection := strings.Split(errPrefix, "\n")
 
 	lCollection := len(errPrefixContextCollection)
-	lastIdx := lCollection - 1
+
+	if lCollection == 0 {
+		return ""
+	}
 
 	var b1 strings.Builder
+	b1.Grow(1024)
+	var funcNames = make([]string, 0, 100)
+	var contextStrs = make([]string, 0, 100)
 	var contextIdx int
-	var s string
 
-	for i := 0; i < lCollection; i++ {
+	for _, s := range errPrefixContextCollection {
 
-		s = errPrefixContextCollection[i]
+		s = strings.TrimLeft(strings.TrimRight(s, " "), " ")
+		s = strings.TrimLeft(strings.TrimRight(s, "\n"), "\n")
+
+		if len(s) == 0 {
+			continue
+		}
 
 		contextIdx = strings.Index(s, " : ")
 
 		if contextIdx == -1 {
-			s = strings.TrimLeft(strings.TrimRight(s, " "), " ")
-
-			b1.WriteString(s + "\n")
-
+			funcNames = append(funcNames, s)
+			contextStrs = append(contextStrs, "0")
 		} else {
 
-			ePrefix := s[0:contextIdx]
-			eContext := s[contextIdx+2:]
+			funcNames = append(funcNames, s[0:contextIdx])
+			contextStrs = append(contextStrs, s[contextIdx+2:])
+		}
 
-			if uint(contextIdx+len(eContext)+3) > maxErrStringLength {
-				b1.WriteString(ePrefix + "\n : " + eContext)
+	}
+
+	lCollection = len(funcNames)
+
+	if lCollection == 0 {
+		return ""
+	}
+
+	var contextStr, funcName string
+	lastStr := ""
+	lastIdx := lCollection - 1
+
+	for i := 0; i < lCollection; i++ {
+		funcName = funcNames[i]
+		contextStr = contextStrs[i]
+
+		if contextStr == "0" {
+			contextStr = ""
+		}
+
+		if len(funcName) == 0 {
+			continue
+		}
+
+		if len(contextStr) == 0 {
+			// len(funcName) > 0 len(contextStr) == 0
+
+			if uint(len(lastStr)+len(funcName)+3) > maxErrStringLength {
+
+				if len(lastStr) > 0 {
+
+					b1.WriteString(lastStr + "\n")
+					lastStr = ""
+
+					if uint(len(funcName)) > maxErrStringLength {
+						b1.WriteString(funcName)
+					} else {
+						// uint(len(funcName)) <= maxErrStringLength
+						if uint(len(funcName)) > maxErrStringLength {
+							b1.WriteString(funcName)
+						} else {
+
+							if len(lastStr) > 0 {
+								lastStr += " - " + funcName
+							} else {
+								lastStr = funcName
+							}
+
+							continue
+						}
+					}
+				} else {
+					// len(lastStr) == 0
+					if uint(len(funcName)) > maxErrStringLength {
+						b1.WriteString(funcName)
+					} else {
+						// len(lastStr) > 0
+						if uint(len(funcName)) > maxErrStringLength {
+							b1.WriteString(funcName)
+						} else {
+							// uint(len(funcName)) <= maxErrStringLength
+							// And len(lastStr) == 0
+							lastStr = funcName
+							continue
+						}
+					}
+				}
 			} else {
+				// uint(len(lastStr) + len(funcName) + 3) <= maxErrStringLength
 
-				b1.WriteString(ePrefix + " : " + eContext)
+				if len(lastStr) > 0 {
+					lastStr += " - " + funcName
+				} else {
+					lastStr = funcName
+				}
+
+				continue
+			}
+		} else {
+			// funcName Len > 0 && contextStr Len > 0
+
+			if uint(len(lastStr)+3+len(funcName)+3+len(contextStr)) > maxErrStringLength {
+
+				if len(lastStr) > 0 {
+					b1.WriteString(lastStr + "\n")
+					lastStr = ""
+				}
+
+				if uint(len(funcName)+3+len(contextStr)) > maxErrStringLength {
+					b1.WriteString(funcName + "\n" + " : " + contextStr)
+				} else {
+					// uint(len(funcName) + 3 + len(contextStr)) <= maxErrStringLength
+					lastStr = funcName + " : " + contextStr
+					continue
+				}
+			} else {
+				//uint(len(lastStr) + 3 + len(funcName) + 3 + len(contextStr)) <= maxErrStringLength
+				if len(lastStr) == 0 {
+					lastStr = funcName + " : " + contextStr
+				} else {
+					lastStr += lastStr + " - " + funcName + " : " + contextStr
+				}
+				continue
 			}
 		}
 
 		if i != lastIdx {
 			b1.WriteString("\n")
 		}
+
+		if b1.Len() > 512 {
+			b1.Grow(1024)
+		}
+
+	}
+
+	if len(lastStr) > 0 {
+		b1.WriteString(lastStr)
 	}
 
 	return b1.String()
