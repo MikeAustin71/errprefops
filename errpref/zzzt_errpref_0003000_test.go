633334a41832ecb7daef0e6212dcf60ccf193b76
Mike Rapp <jordy.rascal@protonmail.com>
2021-02-16 15:53:57 -0600

    Add tests through TestErrPrefixDto_ZEPref_000200()

--- errpref/zzzt_errpref_0003000_test.go
+++ errpref/zzzt_errpref_0003000_test.go
@@ -347,6 +347,14 @@ func TestErrPrefixDto_XEPref_000100(t *testing.T) {
 			"Instead, actualStr= '%v'\n",
 			expectedStr,
 			actualStr)
+
+		return
+	}
+
+	if !ePDto.Equal(xEPDto) {
+		t.Error("Error:\n" +
+			"Expected ePDto to Equal zEPDto2.\n" +
+			"However, THEY ARE NOT EQUAL!!!\n")
 	}
 
 }
@@ -452,9 +460,9 @@ func TestErrPrefixDto_ZCtx_000100(t *testing.T) {
 
 	ePDto.SetEPrefOld(initialStr)
 
-	ePDto2 := ePDto.ZCtx("A!=B")
+	zEPDto2 := ePDto.ZCtx("A!=B")
 
-	actualStr := ePDto2.String()
+	actualStr := zEPDto2.String()
 
 	expectedStr = ErrPref{}.ConvertNonPrintableChars(
 		[]rune(expectedStr),
@@ -471,6 +479,13 @@ func TestErrPrefixDto_ZCtx_000100(t *testing.T) {
 			"Instead, actualStr= '%v'\n",
 			expectedStr,
 			actualStr)
+		return
+	}
+
+	if !ePDto.Equal(&zEPDto2) {
+		t.Error("Error:\n" +
+			"Expected ePDto to Equal zEPDto2.\n" +
+			"However, THEY ARE NOT EQUAL!!!\n")
 	}
 
 }
@@ -493,9 +508,9 @@ func TestErrPrefixDto_ZCtx_000200(t *testing.T) {
 
 	ePDto.SetEPrefOld(initialStr)
 
-	ePDto2 := ePDto.ZCtx("A!=B")
+	zEPDto2 := ePDto.ZCtx("A!=B")
 
-	actualStr := ePDto2.String()
+	actualStr := zEPDto2.String()
 
 	expectedStr = ErrPref{}.ConvertNonPrintableChars(
 		[]rune(expectedStr),
@@ -512,6 +527,13 @@ func TestErrPrefixDto_ZCtx_000200(t *testing.T) {
 			"Instead, actualStr= '%v'\n",
 			expectedStr,
 			actualStr)
+		return
+	}
+
+	if !ePDto.Equal(&zEPDto2) {
+		t.Error("Error:\n" +
+			"Expected ePDto to Equal zEPDto2.\n" +
+			"However, THEY ARE NOT EQUAL!!!\n")
 	}
 
 }
@@ -684,6 +706,180 @@ func TestErrPrefixDto_ZCtxEmpty_000200(t *testing.T) {
 	if !ePDto.Equal(&zEPDto2) {
 		t.Error("Error:\n" +
 			"Expected ePDto to Equal zEPDto2.\n" +
+			"However, THEY ARE NOT EQUAL!!!\n")
+	}
+
+}
+
+func TestErrPrefixDto_ZCtxEmpty_000300(t *testing.T) {
+
+	ePDto := ErrPrefixDto{}.New()
+
+	ePDto.SetMaxTextLineLen(40)
+
+	initialStr :=
+		"Tx1.AVeryVeryLongMethodNameCalledSomething() : A->B\nTx2.SomethingElse() : A==B\n" +
+			"Tx3.DoSomething() : A==10\nTx4() : A/10==4 - Tx5()"
+
+	expectedStr := "Tx1.AVeryVeryLongMethodNameCalledSomething()\\n" +
+		"[SPACE]:[SPACE][SPACE]A->B\\n" +
+		"Tx2.SomethingElse()[SPACE]:[SPACE]A==B\\n" +
+		"Tx3.DoSomething()[SPACE]:[SPACE]A==10\\n" +
+		"Tx4()[SPACE]:[SPACE]A/10==4[SPACE]-[SPACE]Tx5()[SPACE]:[SPACE]A!=B"
+
+	ePDto.SetEPrefOld(initialStr)
+
+	zEPDto := ePDto.ZCtx("A!=B")
+
+	actualStr := zEPDto.String()
+
+	expectedStr = ErrPref{}.ConvertNonPrintableChars(
+		[]rune(expectedStr),
+		true)
+
+	actualStr = ErrPref{}.ConvertNonPrintableChars(
+		[]rune(actualStr),
+		true)
+
+	if expectedStr != actualStr {
+
+		t.Errorf("Error:\n"+
+			"Expected actualStr= '%v'\n"+
+			"Instead, actualStr= '%v'\n",
+			expectedStr,
+			actualStr)
+
+		return
+	}
+
+	expectedStr2 := "Tx1.AVeryVeryLongMethodNameCalledSomething()\\n" +
+		"[SPACE]:[SPACE][SPACE]A->B\\n" +
+		"Tx2.SomethingElse()[SPACE]:[SPACE]A==B\\n" +
+		"Tx3.DoSomething()[SPACE]:[SPACE]A==10\\n" +
+		"Tx4()[SPACE]:[SPACE]A/10==4[SPACE]-[SPACE]Tx5()"
+
+	zEPDto2 := zEPDto.ZCtxEmpty()
+
+	actualStr = zEPDto2.String()
+
+	expectedStr2 = ErrPref{}.ConvertNonPrintableChars(
+		[]rune(expectedStr2),
+		true)
+
+	actualStr = ErrPref{}.ConvertNonPrintableChars(
+		[]rune(actualStr),
+		true)
+
+	if expectedStr2 != actualStr {
+
+		t.Errorf("Error:\n"+
+			"Expected actualStr= '%v'\n"+
+			"Instead, actualStr= '%v'\n",
+			expectedStr2,
+			actualStr)
+	}
+
+	if !zEPDto.Equal(&zEPDto2) {
+		t.Error("Error:\n" +
+			"Expected zEPDto to Equal zEPDto2.\n" +
+			"However, THEY ARE NOT EQUAL!!!\n")
+	}
+
+}
+
+func TestErrPrefixDto_ZEPref_000100(t *testing.T) {
+
+	ePDto := ErrPrefixDto{}.New()
+
+	ePDto.SetMaxTextLineLen(40)
+
+	initialStr :=
+		"Tx1.AVeryVeryLongMethodNameCalledSomething() : A->B\nTx2.SomethingElse() : A==B\n" +
+			"Tx3.DoSomething() : A==10\nTx4() : A/10==4"
+
+	expectedStr := "Tx1.AVeryVeryLongMethodNameCalledSomething()\\n" +
+		"[SPACE]:[SPACE][SPACE]A->B\\n" +
+		"Tx2.SomethingElse()[SPACE]:[SPACE]A==B\\n" +
+		"Tx3.DoSomething()[SPACE]:[SPACE]A==10\\n" +
+		"Tx4()[SPACE]:[SPACE]A/10==4[SPACE]-[SPACE]Tx5()"
+
+	ePDto.SetEPrefOld(initialStr)
+
+	zEPDto := ePDto.ZEPref("Tx5()")
+
+	actualStr := zEPDto.String()
+
+	expectedStr = ErrPref{}.ConvertNonPrintableChars(
+		[]rune(expectedStr),
+		true)
+
+	actualStr = ErrPref{}.ConvertNonPrintableChars(
+		[]rune(actualStr),
+		true)
+
+	if expectedStr != actualStr {
+
+		t.Errorf("Error:\n"+
+			"Expected actualStr= '%v'\n"+
+			"Instead, actualStr= '%v'\n",
+			expectedStr,
+			actualStr)
+
+		return
+	}
+
+	if !ePDto.Equal(&zEPDto) {
+		t.Error("Error:\n" +
+			"Expected ePDto to Equal zEPDto.\n" +
+			"However, THEY ARE NOT EQUAL!!!\n")
+	}
+
+}
+
+func TestErrPrefixDto_ZEPref_000200(t *testing.T) {
+
+	ePDto := ErrPrefixDto{}.New()
+
+	ePDto.SetMaxTextLineLen(40)
+
+	initialStr :=
+		"Tx1.AVeryVeryLongMethodNameCalledSomething() : A->B\nTx2.SomethingElse() : A==B\n" +
+			"Tx3.DoSomething() : A==10\nTx4() : A/10==4"
+
+	expectedStr := "Tx1.AVeryVeryLongMethodNameCalledSomething()\\n" +
+		"[SPACE]:[SPACE][SPACE]A->B\\n" +
+		"Tx2.SomethingElse()[SPACE]:[SPACE]A==B\\n" +
+		"Tx3.DoSomething()[SPACE]:[SPACE]A==10\\n" +
+		"Tx4()[SPACE]:[SPACE]A/10==4"
+
+	ePDto.SetEPrefOld(initialStr)
+
+	zEPDto := ePDto.ZEPref("")
+
+	actualStr := zEPDto.String()
+
+	expectedStr = ErrPref{}.ConvertNonPrintableChars(
+		[]rune(expectedStr),
+		true)
+
+	actualStr = ErrPref{}.ConvertNonPrintableChars(
+		[]rune(actualStr),
+		true)
+
+	if expectedStr != actualStr {
+
+		t.Errorf("Error:\n"+
+			"Expected actualStr= '%v'\n"+
+			"Instead, actualStr= '%v'\n",
+			expectedStr,
+			actualStr)
+
+		return
+	}
+
+	if !ePDto.Equal(&zEPDto) {
+		t.Error("Error:\n" +
+			"Expected ePDto to Equal zEPDto.\n" +
 			"However, THEY ARE NOT EQUAL!!!\n")
 	}
 
