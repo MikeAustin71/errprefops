ea811752141858d58489a5e87c84fd08678bd2e8
Mike Rapp <jordy.rascal@protonmail.com>
2021-04-15 03:20:45 -0500

    Added Test TestErrPrefixDto_NewIBasicErrorPrefix()

--- errpref/zzzt_errpref_0002220_test.go
+++ errpref/zzzt_errpref_0002220_test.go
@@ -6,6 +6,118 @@ import (
 	"testing"
 )
 
+func TestErrPrefixDto_NewIBasicErrorPrefix(t *testing.T) {
+
+	var twoDSlice [][2]string
+
+	twoDSlice = make([][2]string, 14)
+
+	twoDSlice[0][0] = "Tx1.Something()"
+	twoDSlice[0][1] = ""
+
+	twoDSlice[1][0] = "Tx2.SomethingElse()"
+	twoDSlice[1][1] = ""
+
+	twoDSlice[2][0] = "Tx3.DoSomething()"
+	twoDSlice[2][1] = ""
+
+	twoDSlice[3][0] = "Tx4()"
+	twoDSlice[3][1] = ""
+
+	twoDSlice[4][0] = "Tx5()"
+	twoDSlice[4][1] = ""
+
+	twoDSlice[5][0] = "Tx6.DoSomethingElse()"
+	twoDSlice[5][1] = ""
+
+	twoDSlice[6][0] = "Tx7.TrySomethingNew()"
+	twoDSlice[6][1] = "something->newSomething"
+
+	twoDSlice[7][0] = "Tx8.TryAnyCombination()"
+	twoDSlice[7][1] = ""
+
+	twoDSlice[8][0] = "Tx9.TryAHammer()"
+	twoDSlice[8][1] = "x->y"
+
+	twoDSlice[9][0] = "Tx10.X()"
+	twoDSlice[9][1] = ""
+
+	twoDSlice[10][0] = "Tx11.TryAnything()"
+	twoDSlice[10][1] = ""
+
+	twoDSlice[11][0] = "Tx12.TryASalad()"
+	twoDSlice[11][1] = ""
+
+	twoDSlice[12][0] = "Tx13.SomeFabulousAndComplexStuff()"
+	twoDSlice[12][1] = ""
+
+	twoDSlice[13][0] = "Tx14.MoreAwesomeGoodness"
+	twoDSlice[13][1] = "A=7 B=8 C=9"
+
+	iBasicPref := testIBasicErrPref{}
+
+	var err error
+
+	err = iBasicPref.SetEPrefStrings(
+		twoDSlice)
+
+	if err != nil {
+		t.Errorf("Error from iBasicPref.SetEPrefStrings()\n"+
+			"%v\n", err.Error())
+		return
+	}
+
+	var ePDto *ErrPrefixDto
+
+	ePDto,
+		err = ErrPrefixDto{}.NewIBasicErrorPrefix(
+		&iBasicPref,
+		"",
+		"")
+
+	if err != nil {
+		t.Errorf("Error returned by NewIBasicErrorPrefix()\n"+
+			"%v\n",
+			err.Error())
+
+		return
+	}
+
+	ePDto.SetMaxTextLineLen(40)
+
+	ePDto2 := ErrPrefixDto{}.Ptr()
+
+	ePDto2.SetEPrefStrings(twoDSlice)
+
+	ePDto2.SetMaxTextLineLen(40)
+
+	ePDtoStr := ErrPref{}.ConvertNonPrintableChars(
+		[]rune(ePDto.String()),
+		true)
+
+	ePDto2Str := ErrPref{}.ConvertNonPrintableChars(
+		[]rune(ePDto2.String()),
+		true)
+
+	if !ePDto.Equal(ePDto2) {
+		t.Errorf("Error: Expected ePDto==ePDto2.\n"+
+			"However, THEY ARE NOT EQUAL!\n"+
+			"ePDto=\n%v\n\nePDto2=\n%v\n\n",
+			ePDtoStr,
+			ePDto2Str)
+		return
+	}
+
+	if ePDtoStr != ePDto2Str {
+		t.Errorf("Error: Expected ePDtoStr==ePDto2Str.\n"+
+			"However, THEY ARE NOT EQUAL!\n"+
+			"ePDto=\n%v\n\nePDto2=\n%v\n\n",
+			ePDtoStr,
+			ePDto2Str)
+	}
+
+}
+
 func TestErrPrefixDto_NewEPrefCollection(t *testing.T) {
 
 	expectedStr :=
