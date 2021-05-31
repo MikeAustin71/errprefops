466f4233fc53f3509fc5ae24a7e2fda79e5d6a74
Mike Rapp <jordy.rascal@protonmail.com>
2021-05-31 00:02:29 -0500

    Added tests for ErrPrefixDto.ClearLeadingTextStr()

--- errpref/zzzt_errpref_0003500_leadingtext_test.go
+++ errpref/zzzt_errpref_0003500_leadingtext_test.go
@@ -137,3 +137,56 @@ func TestErrPrefixDto_GetTrailingTextStr_000100(t *testing.T) {
 	}
 
 }
+
+func TestErrPrefixDto_ClearLeadingTextStr_000100(t *testing.T) {
+
+	ePDto := ErrPrefixDto{}.New()
+
+	maxLineLen := 50
+
+	ePDto.SetMaxTextLineLen(maxLineLen)
+
+	twoDSlice := make([][2]string, 2)
+
+	twoDSlice[0][0] = "Tx1.Something()"
+	twoDSlice[0][1] = ""
+
+	twoDSlice[1][0] = "Tx2.SomethingElse()"
+	twoDSlice[1][1] = ""
+
+	ePDto.SetEPrefStrings(twoDSlice)
+
+	leadingText := "\n" +
+		strings.Repeat("-", maxLineLen)
+
+	trailingText := strings.Repeat("-", maxLineLen) +
+		"\n"
+
+	ePDto.SetLeadingTextStr(leadingText)
+	ePDto.SetTrailingTextStr(trailingText)
+	ePDto.SetIsLastLineTermWithNewLine(true)
+
+	actualLeadingTxt := ePDto.GetLeadingTextStr()
+
+	if leadingText != actualLeadingTxt {
+		t.Errorf("ERROR: Expected Leading Text String DOES "+
+			"NOT MATCH\nActual Leading Text String!\n"+
+			"Expected Leading Text String= '%v'\n"+
+			"  Actual Leading Text String= '%v'\n",
+			leadingText,
+			actualLeadingTxt)
+		return
+	}
+
+	ePDto.ClearLeadingTextStr()
+
+	actualLeadingTxt = ePDto.GetLeadingTextStr()
+
+	if len(actualLeadingTxt) != 0 {
+		t.Errorf("ERROR: After calling ePDto.ClearLeadingTextStr(),"+
+			"the leading text string NOT EMPTY!\n"+
+			"Actual Leading Text String= '%v'\n",
+			actualLeadingTxt)
+	}
+
+}
